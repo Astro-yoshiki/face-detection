50191a2f49decc4b4b386807efa064314d0a6e5b
YOSHIKI Inoue <white0107rose@gmail.com>
2021-07-21 11:48:28 +0900

    SUBSCRIPTION_KEYを追記

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ st.title("顔認識アプリ")
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
-    api = FaceAPI(img_src=uploaded_file)
+    api = FaceAPI(img_src=uploaded_file, SUBSCRIPTION_KEY=st.secrets["SUBSCRIPTION_KEY"])
     img = api.face_detect()
     st.image(img, caption="Uploaded Image", use_column_width=True)
